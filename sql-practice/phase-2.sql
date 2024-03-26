b9698c0345549bc8f9adffc93ddd83e6e38d3bf7
Victor Alcaraz <vctralcaraz@gmail.com>
2024-03-25 18:01:48 -0700

    phase 2 complete

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,18 @@
 -- Your code here
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS coffee_orders;
+
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(40) NOT NULL,
+  phone DECIMAL(10,0) UNIQUE,
+  email VARCHAR(255) UNIQUE,
+  points INTEGER NOT NULL DEFAULT 5,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE coffee_orders (
+  id INTEGER PRIMARY KEY,
+  is_redeemed BOOLEAN DEFAULT 0,
+  ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
